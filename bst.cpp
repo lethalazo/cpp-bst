18b1cc7f9106fe58ac90e658d8315bd6f8bc8e8e
Arsalan Azmi <mrlethalazo@gmail.com>
2019-03-18 12:56:39 +0000

    Using smart pointers

--- bst.cpp
+++ bst.cpp
@@ -1,6 +1,7 @@
 // Implemented a "sorted" binary tree and a traversal algorithm
 #include <iostream>
 #include <string>
+#include <memory>
 
 using namespace std;
 
@@ -50,30 +51,30 @@ class Node{
 //makes an array of given size, fills it, inserts every element in a bst and prints the tree.
 void fill_and_print(unsigned int size){
   int values[size];
-  Node root(values[0]);
+  unique_ptr<Node> root(new Node(values[0]));
   srand((unsigned)time(0));
   
   for(unsigned int i=0; i<size; i++){ 
       values[i] = (rand()%100)+1;
-      if(i == 0) root.set_value(values[i]);
-      else root.insert(values[i]);
+      if(i == 0) root->set_value(values[i]);
+      else root->insert(values[i]);
   }
   
-  root.printTree();
+  root->printTree();
   delete[] values;
 }
 
 int main()
 {
-  Node root(15);
-  root.insert(12);
-  root.insert(15);
-  root.insert(16);
-  root.insert(13);
-  root.insert(19);
-  root.insert(21);
-  root.insert(0);
-  root.printTree();
+  unique_ptr<Node> root(new Node(15));
+  root->insert(12);
+  root->insert(15);
+  root->insert(16);
+  root->insert(13);
+  root->insert(19);
+  root->insert(21);
+  root->insert(0);
+  root->printTree();
   cout << "\n" << "Printing a random sorted binary tree: ";
   fill_and_print(30); //prints a sorted tree of 30 elements
 }
